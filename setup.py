fda0454edc21c8208cc162492570274299a5d92f
Marcus Maldonado <44212170+marcus-cse@users.noreply.github.com>
2023-01-25 20:59:02 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
+from setuptools import setup
 
+setup(name = 'kmeans-feature-importance',
+      version = '1.0',
+      description = 'kmeans_interp is a wrapper around sklearn.cluster.KMeans which adds the property feature_importances_ that will act as a cluster-based feature weighting technique. Features are weighted using either of the two methods: wcss_min or unsup2sup.',
+      author = 'Yousef Alghofaili',
+      url = 'https://github.com/YousefGh/kmeans-feature-importance',
+      install_requires = ['scikit-learn>=0.24.2',
+                          'numpy'])
